ab87d7823a076b6d01b40295b53253fc12bccc8a
greta <greta.belegu@gmail.com>
2025-02-14 15:31:04 -0500

    initalize

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './index.css';
+
+
+function App() {
+
+  return (
+
+  <div className='bg-glass w-4/5 text-white mx-auto mt-20 rounded-3xl border-3 drop-shadow-lg grid grid-cols-2 gap-4'>
+      <div className='bg-glass w-2/5 text-navy'>
+        <h1 className='text-3xl font-lora font-bold text-center mt-4 mb-8'>Bookshelf</h1>
+      </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}' // Include all relevant file types that will contain Tailwind classes
+  ],
+    corePlugins: {
+    apply: true,
+  },
+  theme: {
+    extend: {
+      colors: {
+
+      },
+    },
+  },
+  plugins: [],
+}
